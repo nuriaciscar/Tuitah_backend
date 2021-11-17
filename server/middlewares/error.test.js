09e2a11c63e03e44d75f0631465b863412ceaf98
Nuria Ciscar <nuriaciscar@MacBook-Pro-de-MacBook.local>
2021-11-17 19:31:11 +0100

    Tested generalError function

--- server/middlewares/error.test.js
+++ server/middlewares/error.test.js
@@ -1,4 +1,5 @@
-const { notFoundError } = require("./error");
+const { ValidationError } = require("express-validation");
+const { notFoundError, generalError } = require("./error");
 
 const mockResponse = () => {
   const res = {};
@@ -19,6 +20,40 @@ describe("Given a notFoundError function", () => {
 
       expect(res.status).toHaveBeenCalledWith(404);
       expect(res.json).toHaveBeenCalledWith(expectedError);
+    });
+  });
+});
+
+describe("Given a generalError function", () => {
+  describe("When it gets a request and an error but no error code", () => {
+    test("Then it should respond with a message 'All broken' and a status code of 500", () => {
+      const error = { error: "All broken" };
+      const res = mockResponse();
+      const req = {};
+      const next = () => {};
+
+      generalError(error, req, res, next);
+
+      expect(res.status).toHaveBeenCalledWith(500);
+      expect(res.json).toHaveBeenCalledWith(error);
+    });
+  });
+  describe("When it gets a request and a ValidationError", () => {
+    test("Then it should respond with a message 'Sorry, bad request' and a status code of 400", () => {
+      const res = mockResponse();
+
+      const error = new ValidationError("details", {
+        error: new Error(),
+        statusCode: 400,
+      });
+
+      const req = {};
+      const next = () => {};
+
+      generalError(error, req, res, next);
+
+      expect(res.status).toHaveBeenCalledWith(400);
+      expect(res.json).toHaveBeenCalledWith({ error: "Sorry, bad request" });
     });
   });
 });
