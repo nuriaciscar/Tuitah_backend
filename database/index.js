d5f59a37048dbb06edb50c8060efce3b852037d0
David Galvez <davidgalvezgomez01@gmail.com>
2021-11-17 21:24:03 +0100

    Fixed the problem with the database

--- database/index.js
+++ database/index.js
@@ -1,38 +1,32 @@
-const mongoose = require("mongoose");
 const debug = require("debug")("tuits: database");
 const chalk = require("chalk");
+const mongoose = require("mongoose");
 
-
-
-const initializeDB = (connectionString) =>{
-
-
+const initializeDB = (connectionString) =>
   new Promise((resolve, reject) => {
-     mongoose.set("toJSON", {
-      virtuals: true,
-      transform: (doc, ret) => {
-        delete ret._id;
-        delete ret._v;
-      },
-
-    });
-
     mongoose.connect(connectionString, (error) => {
       if (error) {
         debug(chalk.red("Cannot connect to database"));
         debug(chalk.red(error.message));
         reject(error);
+        return;
       }
       debug(chalk.green("Connection to database successful!"));
       resolve();
     });
-
-     mongoose.connection.on("close", () => {
+    mongoose.set("debug", true);
+    mongoose.set("toJSON", {
+      virtuals: true,
+      transform: (doc, ret) => {
+        // eslint-disable-next-line no-underscore-dangle
+        delete ret._id;
+        // eslint-disable-next-line no-underscore-dangle
+        delete ret._v;
+      },
+    });
+    mongoose.connection.on("close", () => {
       debug(chalk.red("Connection to database is over"));
+    });
   });
 
-   
-    });
-  };
-
-  module.exports= {initializeDB;}
+module.exports = initializeDB;
--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const { initializeDB } = require("./database");
-const { initializeServer } = require("./server");
-
 require("dotenv").config();
+const initializeDB = require("./database/index");
+const { initializeServer } = require("./server/index");
 
 const port = process.env.PORT ?? process.env.SERVER_PORT ?? 5000;
 
 (async () => {
   try {
-    await initializeDB(process.env.MONGODB_STRING);
     await initializeServer(port);
+    await initializeDB(process.env.MONGODB_STRING);
   } catch (error) {
     process.exit(1);
   }
