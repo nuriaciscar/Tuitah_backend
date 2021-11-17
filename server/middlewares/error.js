60dfdb3dbd5a866bb70b5d1d3598f8ea6623d285
Nuria Ciscar <nuriaciscar@MacBook-Pro-de-MacBook.local>
2021-11-17 19:19:20 +0100

    Created general error function

--- server/middlewares/error.js
+++ server/middlewares/error.js
@@ -6,4 +6,13 @@ const notFoundError = (req, res) => {
   res.status(404).json({ error: "Endpoint not found..." });
 };
 
-module.exports = { notFoundError };
+const generalError = (error, req, res, next) => {
+  debug(chalk.bgMagentaBright("An error has ocurred: ", error.message));
+  if (error instanceof ValidationError) {
+    error.code = 400;
+    error.message = "Sorry, bad request";
+  }
+  const message = error.code ? error.message : "All broken";
+  res.status(error.code || 500).json({ error: message });
+};
+module.exports = { notFoundError, generalError };
