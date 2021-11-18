852cf04fcf9ae2dc02a5ed97d21420640a4c991f
David Galvez <davidgalvezgomez01@gmail.com>
2021-11-18 20:19:55 +0100

    Added route and controller for delete a Tuit

--- server/routes/tuitRoutes.js
+++ server/routes/tuitRoutes.js
@@ -1,9 +1,14 @@
 const express = require("express");
-const { getTuits, createTuit } = require("../controllers/tuitsController");
+const {
+  getTuits,
+  createTuit,
+  deleteTuit,
+} = require("../controllers/tuitsController");
 
 const router = express.Router();
 
 router.get("/", getTuits);
 router.post("/", createTuit);
+router.delete("/:idTuit", deleteTuit);
 
 module.exports = router;
